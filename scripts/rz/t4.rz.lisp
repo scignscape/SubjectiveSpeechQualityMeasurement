7d3d759a2cb6874c035b54515b3dad150f3ec482
ScignScape-RZ <osrworkshops@gmail.com>
2018-11-02 16:41:18 -0400

    update

--- scripts/rz/t4.rz.lisp
+++
@@ -1,7 +0,0 @@
-
- (kb::write-file-start) 
- (kb::write-s0-expression :|_=_generic| :|@%_pr-two-n| :|#__u32| :|###4| :|#__u32| :|###7|) 
- (kb::write-promote-expression) 
- (kb::write-cmd-eval) 
- (kb::write-statement-clear) 
- (kb::write-file-end)
